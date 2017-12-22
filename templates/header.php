1d37a435b7d2d8425b723d67e0cc38380d8fc2a5
Cristiano Maia <criscmaia@gmail.com>
2017-12-22 18:22:51 +0000

    minor tweaks

--- templates/header.php
+++ templates/header.php
@@ -5,11 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Maia Projects</title>
-
-    <title>Família Grizzo . Grice . Gris . Grissi -
-        <?= htmlspecialchars($title) ?>
-    </title>
+    <title><?= htmlspecialchars($title) ?> - Família Grizzo . Grice . Gris . Grissi</title>
 
     <link rel="stylesheet" type="text/css" href="index.css" />
     <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css' rel='stylesheet' type='text/css'>
